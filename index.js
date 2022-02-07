86ef9f6a37cdebcfd31d0a303e17c83c93f28f31
vaibhu4coding <88491711+vaibhu4coding@users.noreply.github.com>
2022-02-07 14:20:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function f() {
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
   document
     .querySelector(".img1")
-    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    .setAttribute("src", "dice" + randomNumber1 + ".png");
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
   document
     .querySelector(".img2")
-    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+    .setAttribute("src", "dice" + randomNumber2 + ".png");
   if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").textContent = "Draw";
   } else if (randomNumber1 > randomNumber2) {
